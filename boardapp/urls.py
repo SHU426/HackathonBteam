2c507f3f546d5cc0ec570cc35688e0e0dff60e34
SHU426 <56482033+SHU426@users.noreply.github.com>
2022-03-01 19:04:53 +0900

    add logout

--- boardapp/urls.py
+++ boardapp/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
-from .views import signupfunc,loginfunc,sredfunc,boardfunc,BoardCreate,BoardDelete
+from .views import logoutfunc, signupfunc,loginfunc,sredfunc,boardfunc,BoardCreate,BoardDelete
 
 urlpatterns = [
-    path('signup/', signupfunc,name='signup'),
+    path('', signupfunc,name='signup'),
     path('login/', loginfunc,name='login'),
     path('sred/', sredfunc, name='sred'),
     path('board/', boardfunc, name='board'),
     path('create/', BoardCreate.as_view(), name='create'),
-    path('delete/<int:pk>', BoardDelete.as_view(), name='delete')
+    path('delete/<int:pk>', BoardDelete.as_view(), name='delete'),
+    path('logout/', logoutfunc,name='logout')
 ]
--- boardapp/views.py
+++ boardapp/views.py
@@ -6,8 +6,9 @@ from django.http import HttpResponse
 from django.shortcuts import render,redirect
 from django.contrib.auth.models import User
 from django.db import IntegrityError 
-from django.contrib.auth import authenticate, login
+from django.contrib.auth import authenticate, login, logout
 from django.urls import reverse_lazy
+from django.contrib.auth.decorators import login_required
 
 from .models import SredModel
 from django.views.generic import CreateView,DeleteView
@@ -30,11 +31,12 @@ def loginfunc(request):
         user = authenticate(request, username=username, password=password)
         if user is not None:
             login(request, user)
-            return render(request,'login.html',{'context':'logged in'})
+            return redirect('sred')
         else:
             return render(request,'login.html',{'context':'not logged in'})
     return render(request,'login.html',{'context':'get method'})
 
+@login_required
 def sredfunc(request):
     object_list = SredModel.objects.all()
     return render(request,'sred.html',{'object_list':object_list})
@@ -55,4 +57,9 @@ class BoardDelete(DeleteView):
     model = SredModel
     fields = ('title','memo')
     success_url = reverse_lazy('sred')
+
+def logoutfunc(request):
+    logout(request)
+    return redirect('login')
+
     
